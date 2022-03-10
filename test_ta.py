21f482628080328479e776074d7952c1aa487a99
Zangir <zangir.iklassov@nu.edu.kz>
2022-03-10 06:13:17 +0400

    Update test_ta.py
    testsize to BS; resume_run boolean added

--- test_ta.py
+++ test_ta.py
@@ -8,22 +8,22 @@ import numpy as np
 import torch
 
 embeddim = 128
-# actorLR = 1e-4
-# criticLR = 1e-4
+actorLR = 1e-4
+criticLR = 1e-4
 masking = 1
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-testsize = 1
 jobs = 15
 ops = 15
 macs = ops
 maxTime = 100
 i = 0
 size_agnostic = True
+resume_run = False
 
 seed = 0
-BS = testsize
+BS = 128
 masking = 1
 
 start = time.time()
@@ -31,12 +31,15 @@ start = time.time()
 
 print('ta{}'.format(i + 1))
 test_precedence, test_timepre_ = read_instances('./data/ta/ta{}'.format(i + 1))
-
 test_timepre = test_timepre_ / maxTime
 
-test_venv = JobShopMultiGymEnv(testsize, jobs, ops, macs)
+# Create batch
+test_precedence = np.repeat(test_precedence, BS, axis=0)
+test_timepre = np.repeat(test_timepre, BS, axis=0)
+
+test_venv = JobShopMultiGymEnv(BS, jobs, ops, macs)
 test_venv.setGame(test_precedence, test_timepre)
-testSamples = [i for i in range(testsize)]
+testSamples = [i for i in range(BS)]
 
 if size_agnostic:
     filedir = './Results/size_agnostic/'
@@ -46,41 +49,39 @@ model_name = 'A2C_Seed%d_BS%d_Mask%d.tar' % (seed, BS, masking)
 if size_agnostic:
     model_name = '%d_%d_100.tar' % (jobs, macs)
 
-checkpoint = torch.load(filedir + model_name, map_location=device)
-
 # Instanitate actor-critic
 actor = Actor(embeddim, jobs, ops, macs, device).to(device)
 critic = Critic6(embeddim, jobs, ops, macs, device).to(device)
 
 # Environment training
 
-actor_opt = optim.Adam(actor.parameters())
-critic_opt = optim.Adam(critic.parameters())
+actor_opt = optim.Adam(actor.parameters(), lr=actorLR)
+critic_opt = optim.Adam(critic.parameters(), lr=criticLR)
 
 filedir = './Results/%dx%d/' % (jobs, macs)
 model_name = 'A2C_Seed%d_BS%d_Mask%d.tar' % (seed, BS, masking)
 
-actor.load_state_dict(checkpoint['actor_state_dict'])
-critic.load_state_dict(checkpoint['critic_state_dict'])
-actor_opt.load_state_dict(checkpoint['actor_opt_state_dict'])
-critic_opt.load_state_dict(checkpoint['critic_opt_state_dict'])
+if resume_run:
+    checkpoint = torch.load(filedir + model_name, map_location=device)
+    actor.load_state_dict(checkpoint['actor_state_dict'])
+    critic.load_state_dict(checkpoint['critic_state_dict'])
+    actor_opt.load_state_dict(checkpoint['actor_opt_state_dict'])
+    critic_opt.load_state_dict(checkpoint['critic_opt_state_dict'])
 
-# actor_opt.param_groups[0]['lr'] = 1e-6
-# critic_opt.param_groups[0]['lr'] = 1e-6
 
 for epi in range(10000):
 
     test_venv.reset(testSamples)
 
     train_rollout = Rollout(test_venv, actor, critic, device, masking)
-    ite, total_reward, States, Log_Prob, Prob, Action, Value, tr_reward, tr_entropy = train_rollout.play(testsize,
+    ite, total_reward, States, Log_Prob, Prob, Action, Value, tr_reward, tr_entropy = train_rollout.play(BS,
                                                                                                          testSamples)
     Log_Prob = torch.stack(Log_Prob)
 
     # !!!!!!!!!!Compute Advantage: Qvalue - Value
     tr_reward.reverse()
 
-    Qvalue = np.zeros((ite, testsize))
+    Qvalue = np.zeros((ite, BS))
     q = 0.0
     for i in range(ite):
         q += np.array(tr_reward[i])  # q.shape=[BS]
